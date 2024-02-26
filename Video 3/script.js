ac0c4e4578bb587224ba9d2be88de22860b4add2
SRI SAIDATTA <nssdatta6@gmail.com>
2024-02-27 02:03:01 +0530

    leant lil bit about buttons

--- Video 3/script.js
+++ Video 3/script.js
@@ -1,1 +1,9 @@
-alert ("Welcome")
+const conclick=document.getElementById("click")
+const title=document.getElementById("title")
+
+
+title.addEventListener(("click"),()=>{
+title.style.color="red"
+title.innerHTML="SUBSCRIBED"
+
+})
